2cafb9ccc15ab6a17b2f79e0cb5ce15089765161
alex <criabiobr@gmail.com>
2023-04-22 10:54:13 -0300

    modeling finding model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Finding(models.Model):
+    HTTP_METHOD_CHOICES = [
+        ('GET', 'GET'),
+        ('POST', 'POST'),
+        ('PUT', 'PUT'),
+        ('DELETE', 'DELETE'),
+        ('PATCH', 'PATCH'),
+        ('OPTIONS', 'OPTIONS'),
+        ('HEAD', 'HEAD'),
+    ]
+
+    target_id = models.IntegerField(unique=True)
+    definition_id = models.IntegerField(unique=True)
+    scans = models.JSONField(default=list, blank=True)
+    url = models.URLField()
+    path = models.URLField()
+    method = models.CharField(max_length=10, choices=HTTP_METHOD_CHOICES, default='INVALID')
