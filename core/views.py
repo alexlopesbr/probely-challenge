c55a3813078729b9cd6466dd5e4c183a4035d804
alex <criabiobr@gmail.com>
2023-04-23 05:32:27 -0300

    view finding

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from django.db.models import Q
 
-# Create your views here.
+from core.models import Finding
+
+
+class FindingListView(View):
+    def get(self, request):
+        queryset = Finding.objects.all()
+
+        definition_id = request.GET.get('definition_id')
+        scan = request.GET.get('scan')
+
+        if definition_id:
+            queryset = queryset.filter(definition_id=definition_id)
+        if scan:
+            queryset = queryset.filter(
+                Q(scans__iregex=f'".*{scan}.*"')
+            )
+
+        data = list(queryset.values())
+        return JsonResponse(data, safe=False)
